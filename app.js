64b042c30ae0c1282a6328bfb6f8e06407072c7c
himanshu-769 <sahuh2305@gmail.com>
2025-02-22 15:51:29 +0530

    this generate new number

---
+++ app.js
@@ -0,0 +1,14 @@
+let genBtn = document.querySelector("button");
+
+genBtn.addEventListener("click", () => {
+    generate();
+});
+
+function generate() {
+    let random = Math.floor(Math.random() * 100) + 1;
+    let h1 = document.querySelector("#show")
+    h1.innerText = random;
+}
+
+
+
